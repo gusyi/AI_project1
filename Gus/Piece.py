605a294fbc595f324ba3b6bfe4123b862c84d60d
gusyin <30853433+gusyi@users.noreply.github.com>
2019-03-31 20:49:06 +1100

    piece class finish

---
+++ Gus/Piece.py
@@ -0,0 +1,35 @@
+## A class representing the different piece on the board (red, green, blue, block
+
+class Piece:
+    # initialise the piece with type and position on the board
+    def __init__(self,type, q, r):
+        self.type = type
+        self.q = q
+        self.r = r
+        
+    # return the type of the piece
+    def getType(self): return self.type
+    
+    #return the Q postion on the board of the piece
+    def getQ(self): return self.q
+    
+    #return the R postion on the board of the piece
+    def getR(self): return self.r
+    
+    #set the type of piece
+    def setType(self, newType):
+        self.type = newType
+        
+    #set the q position of the piece
+    def setQ(self, newQ):
+        self.q = newQ
+    #set the r position   
+    def setR(self, newR):
+        self.r = newR
+    
+    #move the piece to a new position
+    def moveTo(self, newQ, newR):
+        self.q = newQ
+        self.r = newR
+    
+    
